173c51bf79995131ea8331177db8d6fbafead3a4
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-06-14 18:46:22 +0100

    Add Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,11 +8,11 @@ class BankAccount:
         return self.account_balance + amount
 
     def withdraw(self, amount):
-        if amount <= self.account_balance:
-            self.account_balance -= amount
+        if self.account_balance > amount:
+            self.account_balance - amount
             return True
         else:
             return False
 
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance}")
+        print(f"Current balance: ${self.account_balance:.2f}")

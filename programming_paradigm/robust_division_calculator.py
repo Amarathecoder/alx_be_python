1097099b9e4c08355dcca81d8ecf3128be7eff75
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-06-14 19:33:47 +0100

    Add Division Calculator with Cmd Argument

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,8 @@
+def safe_divide(numerator, denominator):
+    try:
+        result = float(numerator) / float(denominator)
+        return f"The result of dividing {numerator} by {denominator} is: {result}"
+    except ZeroDivisionError:
+        return "Error: You can't divide by zero."
+    except ValueError:
+        return "Error: Please enter valid integers."

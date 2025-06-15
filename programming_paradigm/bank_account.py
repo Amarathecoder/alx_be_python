508cb1add4cc0cca6aa972e7f210e12e81b87db4
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-06-15 22:03:49 +0100

    Add Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,4 @@
 class BankAccount:
-
     def __init__(self, account_balance, initial_balance=0):
         self.account_balance = account_balance
         self.initial_balance = initial_balance
@@ -13,6 +12,6 @@ class BankAccount:
             return True
         else:
             return False
-
+        
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")

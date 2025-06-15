9e1b3e6c1c6fd60746c3ddef1b99b16837cfbfaa
Amarathecoder <amarachukwuekwugha@gmail.com>
2025-06-15 22:29:10 +0100

    Delete programming_paradigm/robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++
@@ -1,8 +0,0 @@
-def safe_divide(numerator, denominator):
-    try:
-        result = float(numerator) / float(denominator)
-        return f"The result of dividing by is: {result}"
-    except ZeroDivisionError:
-        return "Error: You can't divide by zero."
-    except ValueError:
-        return "Error: Please enter valid integers."
